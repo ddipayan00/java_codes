b0c58a6dfa7d125d12d936b109823ef31ea9cc68
ddipayan00 <47278363+ddipayan00@users.noreply.github.com>
2021-11-12 22:33:16 +0530

    Update BinarySearchAlgo.java

--- BinarySearchAlgo.java
+++ BinarySearchAlgo.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.Scanner;
-public class Main {
+public class BinarySearchAlgo {
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
 		int[] arr = {1,3,2,6,4,5,7,-45,0,5,9,2};
